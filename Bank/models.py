9c06dfdcf1072edab8a119824249e8c4e2713458
Shamna. K <shamnaisham46@gmail.com>
2021-10-09 22:20:02 +0530

    Add files via upload

---
+++ Bank/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+
+class Data(models.Model):
+    name = models.CharField(max_length=20)
+    phone = models.CharField(max_length=15)
+    address = models.TextField(max_length=50)
+    bloodgroup = models.CharField(max_length=10)
